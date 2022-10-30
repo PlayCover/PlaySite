6014401671b6666a80e72c16b6dc201e96e803bf
Nabrious <103531839+nabriouso@users.noreply.github.com>
2022-10-30 17:57:50 +0100

    Update statics.ts

--- src/utils/statics.ts
+++ src/utils/statics.ts
@@ -1,27 +1,27 @@
 const featuredGames = [
   {
+    name: 'Genshin Impact',
+    link: 'https://is4-ssl.mzstatic.com/image/thumb/Purple112/v4/19/65/ec/1965ec30-c2f0-60e8-53ef-796c9d918cd9/AppIcon-1x_U007emarketing-0-7-0-0-85-220.png/246x0w.webp',
+    classes: 'rounded-xl',
+  },
+  {
+    name: 'Diablo Immortal',
+    link: 'https://is5-ssl.mzstatic.com/image/thumb/Purple122/v4/27/b7/87/27b7872b-2cd4-be39-a931-8f7c114fbe41/AppIcon-1x_U007emarketing-0-10-0-85-220.png/246x0w.webp',
+    classes: 'rounded-xl',
+  },
+  {
     name: 'Lineage 2M',
-    link: 'https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/94/07/6a/94076a2c-ad81-c118-9b77-bd79054017d5/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/460x0w.webp',
+    link: 'https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/f1/43/03/f1430319-f919-20ad-752a-84fb1ed98efb/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp',
     classes: 'rounded-xl',
   },
   {
-    name: 'FINAL FANTASY BRAVE EXVIUS',
-    link: 'https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/ca/16/86/ca1686a8-f7e5-c5ae-b109-5b7bef8ae61e/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/460x0w.webp',
+    name: 'Shadow Fight Arena',
+    link: 'https://is2-ssl.mzstatic.com/image/thumb/Purple112/v4/71/3a/70/713a70ae-e824-21e8-9a21-58f46bd075b7/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp',
     classes: 'rounded-xl',
   },
   {
-    name: 'Blue Archive',
-    link: 'https://is4-ssl.mzstatic.com/image/thumb/Purple112/v4/89/ec/a8/89eca87d-6e29-1b49-03cc-702c73aa19b5/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/460x0w.webp',
-    classes: 'rounded-xl',
-  },
-  {
-    name: 'Geometry Dash',
-    link: 'https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/b6/5d/7b/b65d7be5-e14a-433f-bb53-8cc2407e6199/AppIcon-1x_U007emarketing-85-220-9.png/460x0w.png',
-    classes: 'rounded-xl',
-  },
-  {
-    name: 'Princess Connect! Re: Dive',
-    link: 'https://is4-ssl.mzstatic.com/image/thumb/Purple126/v4/83/80/f3/8380f3cd-4cb5-c33f-49be-3c1dcfc4c4c4/AppIcon-1x_U007emarketing-0-7-0-85-220.png/460x0w.webp',
+    name: 'LoL: Wild Rift',
+    link: 'https://is1-ssl.mzstatic.com/image/thumb/Purple122/v4/17/aa/85/17aa85d7-9b6a-2e3a-5ce8-48ca038034f3/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp',
     classes: 'rounded-xl',
   },
   {
@@ -30,18 +30,18 @@ const featuredGames = [
     classes: 'rounded-xl',
   },
   {
-    name: 'Diablo Inmortal',
-    link: 'https://is2-ssl.mzstatic.com/image/thumb/Purple112/v4/5c/b1/2b/5cb12ba6-fed3-9477-e2aa-462a808e2536/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/460x0w.webp',
+    name: 'Wild Arena Survivors',
+    link: 'https://is3-ssl.mzstatic.com/image/thumb/Purple112/v4/55/a1/b5/55a1b53b-a77b-fa63-48a2-bc580e318374/AppIcon-1x_U007emarketing-0-7-0-85-220.png/246x0w.webp',
     classes: 'rounded-[21%]',
   },
   {
-    name: 'Mahjong Soul',
-    link: 'https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/58/e1/f8/58e1f894-958a-e70e-c6fd-d22ab17c26d9/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/460x0w.webp',
+    name: 'Vainglory',
+    link: 'https://is4-ssl.mzstatic.com/image/thumb/Purple122/v4/cb/77/36/cb773669-2223-ed03-1cfd-cdcc93c16f5e/AppIcon-1x_U007emarketing-0-9-0-85-220.png/246x0w.webp',
     classes: 'rounded-xl',
   },
   {
-    name: 'Genshin Impact',
-    link: 'https://is4-ssl.mzstatic.com/image/thumb/Purple116/v4/fa/76/14/fa761407-4ba2-396e-630e-0dd0b5ffeeee/AppIcon-1x_U007emarketing-0-7-0-0-85-220.png/460x0w.webp',
+    name: 'Rocket League Sideswipe',
+    link: 'https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/31/44/7b/31447b8b-6489-354b-e0db-34c957d50019/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp',
     classes: 'rounded-xl',
   },
 ]
