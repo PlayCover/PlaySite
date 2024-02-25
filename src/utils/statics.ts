fcbead2aacaa08a3ac26b2de9a7edc6bcf12e1eb
zanderp25 <zanderpicon@outlook.com>
2024-02-25 01:13:50 -0500

    fix border radius on app icons (#38)

--- src/utils/statics.ts
+++ src/utils/statics.ts
@@ -2,47 +2,38 @@ const featuredGames = [
   {
     name: 'Genshin Impact',
     link: 'https://is4-ssl.mzstatic.com/image/thumb/Purple112/v4/19/65/ec/1965ec30-c2f0-60e8-53ef-796c9d918cd9/AppIcon-1x_U007emarketing-0-7-0-0-85-220.png/246x0w.webp',
-    classes: 'rounded-xl',
   },
   {
     name: 'Diablo Immortal',
     link: 'https://is5-ssl.mzstatic.com/image/thumb/Purple122/v4/27/b7/87/27b7872b-2cd4-be39-a931-8f7c114fbe41/AppIcon-1x_U007emarketing-0-10-0-85-220.png/246x0w.webp',
-    classes: 'rounded-xl',
   },
   {
     name: 'Lineage 2M',
     link: 'https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/f1/43/03/f1430319-f919-20ad-752a-84fb1ed98efb/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp',
-    classes: 'rounded-xl',
   },
   {
     name: 'Shadow Fight Arena',
     link: 'https://is2-ssl.mzstatic.com/image/thumb/Purple112/v4/71/3a/70/713a70ae-e824-21e8-9a21-58f46bd075b7/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp',
-    classes: 'rounded-xl',
   },
   {
     name: 'LoL: Wild Rift',
     link: 'https://is1-ssl.mzstatic.com/image/thumb/Purple122/v4/17/aa/85/17aa85d7-9b6a-2e3a-5ce8-48ca038034f3/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp',
-    classes: 'rounded-xl',
   },
   {
     name: 'Honkai Impact 3rd',
     link: 'https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/e5/78/4c/e5784c43-a932-5718-41e5-31f88d652de3/AppIcon-1x_U007emarketing-0-9-0-0-85-220.png/460x0w.webp',
-    classes: 'rounded-xl',
   },
   {
     name: 'Wild Arena Survivors',
     link: 'https://is3-ssl.mzstatic.com/image/thumb/Purple112/v4/55/a1/b5/55a1b53b-a77b-fa63-48a2-bc580e318374/AppIcon-1x_U007emarketing-0-7-0-85-220.png/246x0w.webp',
-    classes: 'rounded-[21%]',
   },
   {
     name: 'Vainglory',
     link: 'https://is4-ssl.mzstatic.com/image/thumb/Purple122/v4/cb/77/36/cb773669-2223-ed03-1cfd-cdcc93c16f5e/AppIcon-1x_U007emarketing-0-9-0-85-220.png/246x0w.webp',
-    classes: 'rounded-xl',
   },
   {
     name: 'Rocket League Sideswipe',
     link: 'https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/31/44/7b/31447b8b-6489-354b-e0db-34c957d50019/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp',
-    classes: 'rounded-xl',
   },
 ];
 
